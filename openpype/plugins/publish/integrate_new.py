53803f0f966739dd1c205b3b328eabd54b91620a
Jérôme LORRAIN <jerome.lorrain@protonmail.com>
2021-11-04 15:09:06 +0100

    Hotfix task templating merge

--- openpype/plugins/publish/integrate_new.py
+++ openpype/plugins/publish/integrate_new.py
@@ -172,18 +172,14 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
 
         # Make sure task name in anatomy data is same as on instance.data
         task_name = instance.data.get("task")
-        if task_name:
-            anatomy_data["task"] = task_name
-        else:
-            # Just set 'task_name' variable to context task
-            task_name = anatomy_data["task"]
+        task_info = instance.data.get("task")
 
-        # Find task type for current task name
-        # - this should be already prepared on instance
-        asset_tasks = (
-            asset_entity.get("data", {}).get("tasks")
-        ) or {}
-        task_info = asset_tasks.get(task_name) or {}
+        if task_info:
+            anatomy_data["task"] = task_info
+        else:
+            # Just set 'task_info' variable to context task
+            task_info = anatomy_data["task"]
+
         task_type = task_info.get("type")
         instance.data["task_type"] = task_type
 
@@ -321,7 +317,7 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
 
         key_values = {
             "families": family,
-            "tasks": task_name,
+            "tasks": task_info.get("name"),
             "hosts": instance.context.data["hostName"],
             "task_types": task_type
         }
@@ -804,7 +800,7 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
         #       data?
         #   - should we use context task in that case?
         task_name = (
-            instance.data["anatomyData"]["task"]
+            instance.data["anatomyData"]["task"].get("name")
             or io.Session["AVALON_TASK"]
         )
         task_type = instance.data["task_type"]
